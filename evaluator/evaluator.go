e66cc956fce01b625be42bb1ad4ba91c70b2cb62
Jahkaan Wray <gammawray26@gmail.com>
2023-07-21 08:05:45 +0100

    Added basic evaluation to REPL.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,6 +7,11 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
@@ -14,4 +19,12 @@ func Eval(node ast.Node) object.Object {
 	return nil
 }
 
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
 
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
